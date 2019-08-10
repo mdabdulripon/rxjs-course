7d925b4ea3d6bb22d99b932f7b82ed5f93d7aaaf
mdabdulripon <mdabdulripon@gmail.com>
2019-08-10 16:07:11 -0500

    test(modify Promise behavior without the settimeout.)

--- src/async.js
+++ src/async.js
@@ -1,19 +1,20 @@
 import { add } from './helpers';
 
-add.li(`line 3`);
-// ! Create Promise
-let myPromise = new Promise( (resolve, reject) => {
-    setTimeout( () => {
-        resolve(`Promise is completed`);
-    }, 5000)
+add.li(`line 4`);
+
+const myPromise = new Promise( (resolve, reject) => {
+    resolve(`you are in the promise!`);
+    reject(`there are some error!`);
+});
+
+myPromise.then( res => {
+    add.li(res);
+}).catch( err => {
+    add.li(err);
 })
 
-myPromise
-    .then( res => {
-        add.li(res)
-    }).catch( err => {
-        console.log("TCL: err", err)
-    })
+add.li(`line 20`);
 
-add.li(`below part.....`);
+add.li(`line 30`);
 
+add.li(`line 40`);
