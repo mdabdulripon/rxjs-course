98d2f024d97d2505037944f633484000fe0154a5
mdabdulripon <mdabdulripon@gmail.com>
2019-08-16 18:57:08 -0500

    test(merge creator)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import './reduce'
+import './merge'
 
 document.addEventListener('DOMContentLoaded', function() {
     setTimeout( () => {
-        document.getElementById('title').innerHTML = "RxJs reduce";
+        document.getElementById('title').innerHTML = "RxJs merge";
     }, 5000)
 });
 
